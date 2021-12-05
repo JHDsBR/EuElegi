ad86ee2108b818377182bc1a49ba2a1cc37bcbfd
JHDsBR <Jesse2013.jsc@gmail.com>
2021-12-04 21:12:59 -0300

    votacoes-list

---
+++ src/app/view/proposicoes-list/proposicoes-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proposicoes-list',
+  templateUrl: './proposicoes-list.component.html',
+  styleUrls: ['./proposicoes-list.component.css']
+})
+export class ProposicoesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
